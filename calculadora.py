58db98a7c79a47bd519be1a4e0ab69a68fafa66d
SalvadorGarciaDomingo <salvadorgardom@gmail.com>
2024-04-18 10:50:40 +0200

    editar calculadora

--- calculadora.py
+++ calculadora.py
@@ -1,35 +1,17 @@
-
-from fastapi import FastAPI
-
-#instancia de FastApi
-app=FastAPI()
-
-
 #primer endpoint dirigido a la pagina principal de la api
-@app.get("/")
-def read_root():
-    return{"message": "Bienvenidos a mi API"}
 
-#endpoint suma
-@app.get("/suma/{a},{b}")
 def suma(a: int, b: int):
     suma = a+b
     return suma
 
-#endpoint resta
-@app.get("/resta/{a},{b}")
 def resta(a: int, b: int):
     resta=a-b
     return resta
 
-#endpoint multiplicacion
-@app.get("/multiplicacion/{a},{b}")
 def multiplicacion(a: int, b: int):
     multi=a*b
     return multi
 
-#endpoint division
-@app.get("/division/{a},{b}")
 def division(a: int, b: int):
     if b==0:
         return {"message": "No se puede dividir entre 0"}
@@ -40,3 +22,6 @@ def division(a: int, b: int):
 
 if __name__ == "__main__":
  print(suma(5, 3))
+ print(resta(3, 2))
+ print(multiplicacion(3, 2))
+ print(division(4, 2), 2)
